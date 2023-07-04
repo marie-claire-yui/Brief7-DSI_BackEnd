b25bd79aa8c22b9d2750fa513c57187e47ee9228
elimut <vandingenenannesophie@gmail.com>
2023-07-04 15:56:46 +0200

    test branch

---
+++ src/db/sequelize.js
@@ -0,0 +1,39 @@
+// Ce fichier gère la configuration de la base de données et l'initialisation du modèle User.
+const { Sequelize, DataTypes} = require('sequelize');
+// récupération des prorpiétés de sequelize sequelize et dataypes
+const UserModel = require('../models/users');
+// Importation du modèle d'utilisateur (users.js)  
+
+const sequelize = new Sequelize('dsi_sensibilisation', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Etc/GMT-2',
+  },
+  logging: false
+})
+// Création d'une instance Sequelize avec les informations de connexion à la base de données
+
+
+const User = UserModel(sequelize, DataTypes);
+// Création du modèle User en utilisant le modèle UserModel et les paramètres sequelize et DataTypes
+
+
+
+sequelize
+  .query('SELECT 1')
+  .then(([result]) => {
+    console.log('Connexion à la base de données réussie !');
+  })
+  .catch(error => {
+    console.error('Erreur de connexion à la base de données :', error);
+  });
+// Cette requête sélectionne  la valeur 1 pour vérifier si la connexion à la base de données est réussie.
+
+
+module.exports = { 
+    User
+ }
+// Exportation du modèle User pour pouvoir l'utiliser ailleurs dans le code.
+
+
---
+++ src/routes/deleteUser.js
@@ -0,0 +1,25 @@
+const { User } = require('../db/sequelize');
+
+module.exports = (app) => {
+  app.delete('/user_tests/:id', (req, res) => {
+    User.findByPk(req.params.id).then((user) => {
+      if (!user) {
+        return res.status(404).json({ error: 'Utilisateur non trouvé.' });
+      }
+
+      const userDeleted = user;
+      user.destroy().then(() => {
+        const message = `L'utilisateur n°${userDeleted.id_user} a bien été supprimé.`;
+        res.json({ message, data: userDeleted });
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la suppression de l\'utilisateur:', error);
+        res.status(500).json({ error: 'Une erreur est survenue lors de la suppression de l\'utilisateur.' });
+      });
+    })
+    .catch((error) => {
+      console.error('Erreur lors de la recherche de l\'utilisateur:', error);
+      res.status(500).json({ error: 'Une erreur est survenue lors de la recherche de l\'utilisateur.' });
+    });
+  });
+};
